212298934b95aa13608c23d533679bf49e23e4a7
eduardorg <eduardo.ruiz@landbot.io>
2022-03-22 23:24:06 +0100

    feat: api endpoints definition

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,19 @@
-export const api = {};
+import { CharacterDataResponse, CharactersAPIParams } from './types';
+
+const domain = 'https://gateway.marvel.com';
+
+const getParams = (params: CharactersAPIParams = {}) => {
+  return new URLSearchParams({
+    apikey: process.env.REACT_APP_MARVEL_PUBLIC_API_KEY as string,
+    ...params,
+  });
+};
+
+export const api = {
+  getCharacters: (
+    params?: CharactersAPIParams
+  ): Promise<CharacterDataResponse> =>
+    fetch(domain + '/v1/public/characters?' + getParams(params)).then((res) =>
+      res.json()
+    ),
+};
