32c4a46b1c4a222e379d2dff282b0509cc50c0b2
Caront3 <90094202+Caront3@users.noreply.github.com>
2022-03-29 09:32:13 +0200

    Fetch hook (#9)
    * feat: new custom hook useFetch
    
    * feat: hooks index
    
    * feat: new useFetch custom hook
    
    * feat: new useFetch custom hook
    
    * docs: test comment
    
    * feat: enhaced code understanding

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../../routes';
-import { styled } from '@mui/material';
+import { Container, styled } from '@mui/material';
 import marvelLogo from './Marvel_Logo.png';
 
 export interface HeaderProps {
@@ -29,17 +29,19 @@ export const Header: React.FunctionComponent<HeaderProps> = ({ className }) => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position='static'>
-        <Toolbar>
-          <StyledLink to={ROUTES.HOME}>
-            <img src={marvelLogo} alt='logo' />
-          </StyledLink>
-          <StyledLink to={ROUTES.HOME}>
-            <Button color='inherit'>Inicio</Button>
-          </StyledLink>
-          <StyledLink to={ROUTES.HEROES}>
-            <Button color='inherit'>Heroes</Button>
-          </StyledLink>
-        </Toolbar>
+        <Container>
+          <Toolbar>
+            <StyledLink to={ROUTES.HOME}>
+              <img src={marvelLogo} alt='logo' />
+            </StyledLink>
+            <StyledLink to={ROUTES.HOME}>
+              <Button color='inherit'>Inicio</Button>
+            </StyledLink>
+            <StyledLink to={ROUTES.HEROES}>
+              <Button color='inherit'>Heroes</Button>
+            </StyledLink>
+          </Toolbar>
+        </Container>
       </AppBar>
     </Box>
   );
