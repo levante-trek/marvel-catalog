d34a4b68af42e2a332829fb5fa72f95aa8f1214b
Caront3 <karlosm01@gmail.com>
2022-04-05 18:36:10 +0200

    feat: heroe details modal component

--- src/components/HeroeCard/HeroeCard.tsx
+++ src/components/HeroeCard/HeroeCard.tsx
@@ -1,36 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import { HeroeDetailsModal } from '../HeroeDetailsModal';
 
 export interface HeroeCardProps {
   className?: string;
   name: string;
   image: string;
+  description: string;
 }
 
 export const HeroeCard: React.FunctionComponent<HeroeCardProps> = ({
   className,
   name,
   image,
+  description,
 }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
-    <Card>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="200"
-          image={image}
-          alt="Heroe avatar"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div" align="center">
-            {name}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <>
+      <Card>
+        <CardActionArea onClick={handleOpen}>
+          <CardMedia
+            component='img'
+            height='200'
+            image={image}
+            alt='Heroe avatar'
+          />
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant='h5'
+              component='div'
+              align='center'
+            >
+              {name}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <HeroeDetailsModal
+        open={open}
+        handleClose={handleClose}
+        name={name}
+        description={description}
+        image={image}
+      />
+    </>
   );
 };
